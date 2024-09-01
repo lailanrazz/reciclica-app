ddb1540e38dc581233a5f18da1abcf57db59a8bc
lailanrazz <lailanurazizah@mail.ugm.ac.id>
2024-09-01 12:50:58 +0700

    update video-10-1

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,38 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';  // Impor Router untuk navigasi
-import { FormGroup } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LoginPageForm } from './login.page.form';
-import { Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { hide, show } from 'src/store/loading/loading.actions';
+import { login, recoverPassword } from 'src/store/login/login.actions';
+import { AppState } from 'src/store/AppState';
+import { ToastController } from '@ionic/angular';
+import { LoginState } from 'src/store/login/LoginState';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy {
 
   form!: FormGroup;
+  loginStateSubscription!: Subscription;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) { }  // Injeksi Router
+  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>, private toastController: ToastController) { }
 
   ngOnInit() {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
+    this.form = new LoginPageForm(this.formBuilder).createForm();
+
+    this.loginStateSubscription = this.store.select('login').subscribe(loginState => {
+      this.onIsRecoveredPassword(loginState);
+      this.onIsloggedIn(loginState);
+      this.onError(loginState);
+      this.toggleLoading(loginState);
+    })
   }
 
-  // Method login
-  login() {
-    // Logika login bisa dimasukkan di sini, misalnya validasi input pengguna
+  ngOnDestroy() {
+    if (this.loginStateSubscription) {
+      this.loginStateSubscription.unsubscribe()
+    }
+  }
 
-    // Navigasi ke halaman home setelah login sukses
-    this.router.navigate(['home']);
+  private toggleLoading(loginState: LoginState) {
+    if (loginState.isLoggingIn || loginState.isRecoveringPassword) {
+      this.store.dispatch(show());
+    } else {
+      this.store.dispatch(hide());
+    }
+  }
+
+  private onIsloggedIn(loginState: LoginState){
+    if (loginState.isLoggedIn) {
+      this.router.navigate(['home']);
+    }
+  }
+
+  private async onError(loginState: LoginState) {
+    if (loginState.error) {
+      const toaster = await this.toastController.create({
+        position: "bottom",
+        message: loginState.error.message,
+        color: "danger",
+        duration: 3000 // Display for 3 seconds
+      });
+      toaster.present();
+    }
+  }
+
+  private async onIsRecoveredPassword(loginState: LoginState) {
+    if (loginState.isRecoveredPassword) {
+      const toaster = await this.toastController.create({
+        position: "bottom",
+        message: "Recovery email sent",
+        color: "primary"
+      });
+      toaster.present();
+    }
+  }
+
+  forgotEmailPassword() {
+    this.store.dispatch(recoverPassword({email: this.form.get('email')?.value}));
+  }
+
+  login() {
+    this.store.dispatch(login({email: this.form.get('email')?.value, password: this.form.get('password')?.value}));
   }
 
   register() {
     this.router.navigate(['register']);
   }
-
 }

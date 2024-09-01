6f6989b4155689eec8a21688d66ea32c9668c972
lailanrazz <lailanurazizah@mail.ugm.ac.id>
2024-09-01 13:01:44 +0700

    reciclica-update

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -9,6 +9,7 @@ import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 import { ErrorMessageComponent } from 'src/app/components/error-message/error-message.component';
 
+
 @NgModule({
   imports: [
     CommonModule,
--- src/app/pages/login/login.page.form.spec.ts
+++ src/app/pages/login/login.page.form.spec.ts
@@ -1,43 +1,143 @@
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { LoginPage } from "./login.page";
-import { LoginPageForm } from "./login.page.form";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoginPage } from './login.page';
+import { waitForAsync } from '@angular/core/testing';
+import { IonicModule, ToastController } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import { loadingReducer } from 'src/store/loading/loading.reducers';
+import { loginReducer } from 'src/store/login/login.reducers';
+import { AppState } from 'src/store/AppState';
+import { login, loginFail, loginSuccess, recoverPassword, recoverPasswordFail, recoverPasswordSuccess } from 'src/store/login/login.actions';
+import { User } from 'src/app/model/user/User';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.prod';
 
-describe('LoginPageForm', () => {
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
+  let page: any;
+  let store: Store<AppState>;
+  let toastController: ToastController;
 
-    let loginPageForm: LoginPageForm;
-    let form: FormGroup;
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginPage ],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot([]),
+        StoreModule.forFeature("loading", loadingReducer),
+        StoreModule.forFeature("login", loginReducer),
+        AngularFireModule.initializeApp(environment.firebaseConfig)
+      ]
+    }).compileComponents();
 
-    beforeEach(() => {
-        loginPageForm = new LoginPageForm(new FormBuilder());
-        form = loginPageForm.createForm();
+    fixture = TestBed.createComponent(LoginPage);
+    router = TestBed.get(Router);
+    store = TestBed.get(Store);
+    toastController = TestBed.get(ToastController);
+
+    component = fixture.componentInstance;
+    page = fixture.debugElement.nativeElement;
+  }));
+
+  it('should create form on init', () => {
+    component.ngOnInit();
+
+    expect(component.form).not.toBeUndefined();
+  })
+
+  it('should go to register page on register', () => {
+    spyOn(router, 'navigate');
+
+    component.register();
+
+    expect(router.navigate).toHaveBeenCalledWith(['register']);
+  })
+
+  it('should recover email/password on forgot email/password', () => {
+    fixture.detectChanges();
+    component.form.get('email')?.setValue("valid@email.com");
+    page.querySelector("#recoverPasswordButton").click();
+
+    store.select('login').subscribe(loginState => {
+      expect(loginState.isRecoveringPassword).toBeTruthy();
+    })
+    store.select('loading').subscribe(loadingState => {
+      expect(loadingState.show).toBeTruthy();
+    })
+  })
+
+  it('given user is recovering password, when success, then hide loading and show success message', () => {
+    spyOn(toastController, 'create').and.returnValue(<any> Promise.resolve({present: () => {}}));
+
+    fixture.detectChanges();
+    store.dispatch(recoverPassword({email: "any@email.com"}));
+    store.dispatch(recoverPasswordSuccess());
+    store.select('loading').subscribe(loadingState => {
+      expect(loadingState.show).toBeFalsy();
     })
 
-    it('should create login form empty', () => {
-        
-        expect(form).not.toBeNull();
-        expect(form.get('email')).not.toBeNull();
-        expect(form.get('email')!.value).toEqual("");
-        expect(form.get('email')!.valid).toBeFalsy();
-        expect(form.get('password')).not.toBeNull();
-        expect(form.get('password')!.value).toEqual("");
-        expect(form.get('password')!.valid).toBeFalsy();
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+  })
+
+  it('given user is recovering password, when fail, then hide loading and show error message', () => {
+    spyOn(toastController, 'create').and.returnValue(<any> Promise.resolve({present: () => {}}));
+
+    fixture.detectChanges();
+    store.dispatch(recoverPassword({email: "any@email.com"}));
+    store.dispatch(recoverPasswordFail({error: "message"}));
+    store.select('loading').subscribe(loadingState => {
+      expect(loadingState.show).toBeFalsy();
     })
 
-    it('should have email invalid if email is not valid', () => {
-        form.get('email')!.setValue("test");
-        expect(form.get('email')!.valid).toBeFalsy();
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+  })
+
+  it('should show loading and start login where logging in', () => {
+    fixture.detectChanges();
+    component.form.get('email')?.setValue('valid@email.com');
+    component.form.get('password')?.setValue('anyPassword');
+    page.querySelector('#loginButton').click();
+    store.select('loading').subscribe(loadingState => {
+      expect(loadingState.show).toBeTruthy();
+    })
+    store.select('login').subscribe(loginState => {
+      expect(loginState.isLoggingIn).toBeTruthy();
+    })
+  })
+
+  it('given user is logging in, when success, then hide loading and send user to home page', () => {
+    spyOn(router, 'navigate');
+
+    fixture.detectChanges();
+    store.dispatch(login({email: "valid@email.com", password: "anyPassword"}));
+    store.dispatch(loginSuccess({user: new User()}));
+
+    store.select('loading').subscribe(loadingState => {
+      expect(loadingState.show).toBeFalsy();
+    })
+    store.select('login').subscribe(loginState => {
+      expect(loginState.isLoggedIn).toBeTruthy();
+    })
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  })
+
+  it('given user is logging in, when fail, then hide loading and show error message', () => {
+    spyOn(toastController, 'create').and.returnValue(<any> Promise.resolve({present: () => {}}));
+
+    fixture.detectChanges();
+    store.dispatch(login({email: "valid@email.com", password: "anyPassword"}));
+    store.dispatch(loginFail({error: {message: 'error message'}}));
+
+    store.select('loading').subscribe(loadingState => {
+      expect(loadingState.show).toBeFalsy();
     })
 
-    it('should have email valis if email is valid', () => {
-        form.get('email')!.setValue("valid@gmail.com");
-        expect(form.get('email')!.valid).toBeTruthy();
-    })
-
-    it('should have a valid form', () => {
-        form.get('email')!.setValue("valid@gmail.com");
-        form.get('password')!.setValue("anyPassword");
-
-        expect(form.valid).toBeTruthy();
-    })
-
-})
+    expect(toastController.create).toHaveBeenCalledTimes(1);
+  })
+});

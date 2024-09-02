36c809f0665f6ec1b6eff786c2f3c06aecd06e24
lailanrazz <lailanurazizah@mail.ugm.ac.id>
2024-09-02 10:15:51 +0700

    Update-Auth-guard

--- src/app/guards/auth/auth-guard.service.spec.ts
+++ src/app/guards/auth/auth-guard.service.spec.ts
@@ -1,17 +1,37 @@
 import { TestBed } from '@angular/core/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { User } from 'src/app/model/user/User';
+import { loginReducer } from 'src/store/login/login.reducers';
 import { AuthGuard } from './auth-guard.service';
+import { loginSuccess } from 'src/store/login/login.actions'; // Import the loginSuccess action
+import { AppState } from 'src/store/AppState'; // Ensure this path is correct
 
 describe('AuthGuardService', () => {
   let service: AuthGuard;
+  let store: Store<AppState>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({}),
+        StoreModule.forFeature('login', loginReducer)
+      ]
+    });
+
     service = TestBed.inject(AuthGuard);
+    store = TestBed.inject(Store); // Use inject instead of get
   });
 
   it('should allow logged user to access page', () => {
+    store.dispatch(loginSuccess({ user: new User() })); // Dispatch loginSuccess action
     service.canLoad().subscribe(isAllowed => {
       expect(isAllowed).toBeTruthy();
+    });
+  });
+
+  it('should not allow access to page if user is not logged in', () => {
+    service.canLoad().subscribe(isAllowed => {
+      expect(isAllowed).toBeFalsy();
     });
   });
 });
--- src/app/guards/auth/auth-guard.service.ts
+++ src/app/guards/auth/auth-guard.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanLoad } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { Store } from '@ngrx/store'; // Import Store
-import { AppState } from 'src/store/AppState'; // Corrected import path for AppState
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { take, map } from 'rxjs/operators'; // Import `take` and `map`
+import { AppState } from 'src/store/AppState'; // Ensure this path is correct
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,9 @@ export class AuthGuard implements CanLoad {
   constructor(private store: Store<AppState>) { }
 
   canLoad(): Observable<boolean> {
-    return of(true);
+    return this.store.select('login').pipe(
+      take(1),
+      map(loginState => loginState.isLoggedIn)
+    );
   }
 }

8affd27a30f0f2a8b8b6623149c95d873ef310e0
lailanrazz <lailanurazizah@mail.ugm.ac.id>
2024-09-02 09:55:06 +0700

    update auth-guard & app-routing module

--- src/app/guards/auth/auth-guard.service.ts
+++ src/app/guards/auth/auth-guard.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { CanLoad } from '@angular/router';
+import { Observable, of } from 'rxjs'; 
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuard implements CanLoad {
 
   constructor() { }
+
+  canLoad(): Observable<boolean> {
+    return of(true);
+  }
 }
